9b06f69099fa775459bd9e1b618969749bbda31c
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-05-21 23:28:47 +0200

    wip

--- Main.hs
+++ Main.hs
@@ -1,104 +1,141 @@
-{-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE UndecidableInstances #-}
-
--- TODO
--- * Inject randomness
--- * Express "next larger power of two"
--- * Fix inline TODOs
--- * Fix undefined's
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Main where
+
+import Data.Bool (bool)
+import Data.Proxy (Proxy (Proxy))
+import Prelude hiding (exponent, significand)
 
 class
   ( RealFloat f,
     Eq (Exponent f),
-    Num (Significand f),
-    Eq (Significand f)
+    Eq (Significand f),
+    Num (Exponent f),
+    Num (Significand f)
   ) =>
   IEEERepr f where
   type Exponent f
   type Significand f
 
-  zeroExpt :: f -> Exponent f
+  zeroExponent :: Proxy f -> Exponent f
 
-  expt :: f -> Exponent f
-  sfcd :: f -> Significand f
+  minSignificand :: Proxy f -> Significand f
+  maxSignificand :: Proxy f -> Significand f
+
+  exponent :: f -> Exponent f
+  significand :: f -> Significand f
+
+  assemblePositive :: Proxy f -> (Exponent f, Significand f) -> f
+
+class (Monad m, IEEERepr f) => MonadIEEE m f where
+  drawSign :: Proxy f -> m Bool
+  drawExponent :: Proxy f -> (Exponent f, Exponent f) -> m (Exponent f)
+  drawSignificand ::
+    Proxy f ->
+    (Significand f, Significand f) ->
+    m (Significand f)
+
+perhapsNegate :: forall f m. MonadIEEE m f => m (f -> f)
+perhapsNegate = bool id negate <$> drawSign (Proxy :: Proxy f)
 
 -- | [2^e + x, 2^e + y].
 uniformExponentsEqual ::
-  IEEERepr f =>
+  MonadIEEE m f =>
+  Proxy f ->
   Exponent f ->
   Significand f ->
   Significand f ->
-  f
-uniformExponentsEqual = undefined
+  m f
+uniformExponentsEqual p e sx sy =
+  assemblePositive p . (e,) <$> drawSignificand p (sx, sy)
 
 -- | [2^x, 2^y]
 uniformSignificandsZero ::
-  IEEERepr f =>
+  MonadIEEE m f =>
+  Proxy f ->
   Exponent f ->
   Exponent f ->
-  f
-uniformSignificandsZero = undefined
-
--- | [0, y], assumes 0 < y.
-uniformLeftZero ::
-  IEEERepr f =>
-  f ->
-  f
-uniformLeftZero y
-  | sfcd y == 0 = uniformSignificandsZero (zeroExpt y) (expt y)
-  | otherwise = undefined
+  m f
+uniformSignificandsZero p ex ey = do
+  e <- drawExponent p (ex, ey)
+  s <- drawSignificand p (minSignificand p, maxSignificand p)
+  return $ assemblePositive p (e, s)
 
 -- | [x, y], assumes 0 < x < y.
 uniformPositive ::
-  IEEERepr f =>
+  forall f m.
+  MonadIEEE m f =>
   f ->
   f ->
-  f
+  m f
 uniformPositive x y
-  | expt x == expt y = uniformExponentsEqual (expt x) (sfcd x) (sfcd y)
-  | sfcd x == 0 && sfcd y == 0 = uniformSignificandsZero (expt x) (expt y)
-  | otherwise = undefined
+  | ex == ey = uniformExponentsEqual Proxy ex sx sy
+  | sx == 0 && sy == 0 = uniformSignificandsZero Proxy ex ey
+  | otherwise =
+    let go = do
+          u <- uniformSignificandsZero Proxy ex ey
+          if x <= u && u <= y then return u else go
+     in go
+  where
+    ex = exponent x
+    ey = exponent y
+    sx = significand x
+    sy = significand y
+
+-- | [0, y], assumes 0 < y.
+uniformLeftZero ::
+  forall f m.
+  MonadIEEE m f =>
+  f ->
+  m f
+uniformLeftZero = uniformPositive 0
 
 -- | [x, y], assumes x < 0 < y.
 uniformSpansZero ::
+  forall f m.
+  MonadIEEE m f =>
   f ->
   f ->
-  f
-uniformSpansZero = undefined
+  m f
+uniformSpansZero x y = go
+  where
+    z = max (- x) y
+    go = do
+      u <- perhapsNegate <*> uniformLeftZero z
+      if x <= u && u <= y then return u else go
 
 uniform ::
-  IEEERepr f =>
+  forall f m.
+  MonadIEEE m f =>
   f ->
   f ->
-  f
+  m f
 uniform x y
-  | isNaN x || isNaN y = x
-  | x == y = x
+  | isNaN x || isNaN y = return x
+  | x == y = return x
   | x > y = error "uniform: requires not y < x"
-  | isInfinite x && isInfinite y = x -- TODO: coin flip for sign x or y
-  | isInfinite x = x
-  | isInfinite y = y
-  | y <= 0 = - (uniformRightPositive (- y) (- x))
+  | isInfinite x && isInfinite y = bool x y <$> drawSign p
+  | isInfinite x = return x
+  | isInfinite y = return y
+  | y <= 0 = negate <$> uniformRightPositive (- y) (- x)
   | otherwise = uniformRightPositive x y
+  where
+    p = Proxy :: Proxy f
 
 -- | Assumes x < y and y > 0.
 uniformRightPositive ::
-  IEEERepr f =>
+  forall f m.
+  MonadIEEE m f =>
   f ->
   f ->
-  f
+  m f
 uniformRightPositive x y
   | x == 0 = uniformLeftZero y
-  | x == - y = uniformLeftZero y -- TODO: flip coin for sign
+  | x == - y = perhapsNegate <*> uniformLeftZero y
   | x < 0 = uniformSpansZero x y
   | otherwise = uniformPositive x y
 
