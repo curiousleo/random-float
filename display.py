c51ec8008b3576b8b7d417d9aabaf3d702c034c4
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-06-01 21:39:49 +0200

    Simplify display.py

--- display.py
+++ display.py
@@ -1,31 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def explode(xs):
-    xsabs = np.abs(xs)
-    b_s = 52 if xs.dtype == np.float64 else 23
-    b_e = 11 if xs.dtype == np.float64 else 8
-    bias = 1023 if xs.dtype == np.float64 else 127
-    dtype = np.uint64 if xs.dtype == np.float64 else np.uint32
-
-    ns = xs < 0
-    es = (xsabs.view(dtype) >> b_s) & ((1<<b_e)-1)
-    ss = xsabs.view(dtype) & ((1<<b_s)-1)
-
-    assert(ns.size == xs.size)
-    assert(es.size == xs.size)
-    assert(ss.size == xs.size)
-
-    return (ns, es, ss)
-
 if __name__ == "__main__":
     from sys import stdin
-    xs = np.loadtxt(stdin, dtype=np.float32)
-    #(ns, es, ss) = explode(xs)
-    #print(xs)
-    #print(ns)
-    #print(es)
-    #print(ss)
+    xs = np.loadtxt(stdin, dtype=np.float64)
 
     unique, counts = np.unique(xs, return_counts=True)
     diff = np.ediff1d(unique, to_begin=0, to_end=0) / 2
